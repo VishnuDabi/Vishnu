eb862a922563d24c479390ecede047de7dc39b7c
VishnuDabi <vishnudabi999@gmain.com>
2022-11-09 22:48:25 +0530

    first commit

---
+++ VDRG4598HS/C programming/Array/9Nov22-Decending-array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int size,i,j,c;
+    printf("Enter Size\n");
+    scanf("%d",&size);
+    int ar[size];
+    printf("Enter %d element of Array\n",size);
+    for( i=0;i<size;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=0;i<size;i++)
+    {
+        printf("Array element %d = %d\n",i+1,ar[i]);
+    }
+    for(i=0;i<size;i++)
+    {
+        for(j=i+1;j<size;j++)
+        {
+            if(ar[j]>ar[i])
+            {
+                c=ar[j];
+                ar[j]=ar[i];
+                ar[i]=c;
+            }
+        }
+    }
+    for(i=0;i<size;i++)
+    {
+        printf("Array = %d\n",ar[i]);
+    }
+}
